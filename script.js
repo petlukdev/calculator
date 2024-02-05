b86d11bb274aa1b16e210a8d0b99adf201c11803
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2024-02-05 19:14:56 +0100

    Fixed clear function + added digit limit

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const result = document.querySelector(".box")
+const result = document.getElementById("result")
 const clr = document.getElementById("clr")
 const del = document.getElementById("del")
 const nums = document.querySelectorAll(".num")
 const modifiers = document.querySelectorAll(".action")
 
 clr.addEventListener("click", () => {
-    if (result.value == "") return
-    result.reset()
+    if (result.innerText == "") return
+    result.innerText = ""
 })
 
 nums.forEach((i) =>
     i.addEventListener("click", () => {
-        if (i.textContent == 0 && result.value == "0") return
-        if (result.value == "0") {
-            result.reset()
+        if (result.innerText.length + 1 > 15) return
+        if (i.textContent == 0 && result.innerText == "0") return
+        if (result.innerText == "0") {
+            result.innerText = i.textContent
         } 
         else {
-            result.value += i.textContent
+            result.innerText += i.textContent
         }
     })
 )
