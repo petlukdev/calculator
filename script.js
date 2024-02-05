6e963ec22fc58daa239a96959d207177068d501f
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2024-02-05 19:30:56 +0100

    Added decimal check

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ const del = document.getElementById("del")
 const nums = document.querySelectorAll(".num")
 const modifiers = document.querySelectorAll(".action")
 
+let decimal = false
+
 clr.addEventListener("click", () => {
     if (result.innerText == "") return
     result.innerText = ""
+    decimal = false
 })
 
 nums.forEach((i) =>
     i.addEventListener("click", () => {
         if (result.innerText.length + 1 > 15) return
-        if (i.textContent == 0 && result.innerText == "0") return
+        if (i.textContent == "0" && result.innerText == "0") return
+        if (i.textContent == "." && decimal) return
+        if (i.textContent == ".") decimal = true
+
         if (result.innerText == "0") {
             result.innerText = i.textContent
-        } 
+        }
         else {
             result.innerText += i.textContent
         }
